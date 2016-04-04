4eaaaebc2099294ca4db8a86280b7d4a50e38b81
jmjuanes <josemijuanes@gmail.com>
2016-04-04 14:06:02 +0200

    Automatize icons build using gulp-replace and gulp-rename

--- tasks/build-icons.js
+++ tasks/build-icons.js
@@ -1,57 +1,32 @@
-//Import dependencies
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var path = require('path');
+//Import gulp plugins
+var replace = require('gulp-replace');
+var rename = require('gulp-rename');
 
 //Function for build the icons
-function BuildIcons(output, icons, colors)
+module.exports = function(gulp, icons, colors, output)
 {
-	//Create the putput dir
-	mkdirp.sync(output);
+	//Create the regexp
+	var regexp = new RegExp('#000000', 'g');
 
-	//Get all the categories
-	var cat = Object.keys(icons);
+	//Icons source
+	var src = [];
 
-	//Read all the categories
-	for(var i = 0; i < cat.length; i++)
+	//Read all the icons
+	for(var i = 0; i < icons.length; i++){ src.push('./icons/' + icons[i]); }
+
+	//Read all the colors
+	for(var i = 0; i < colors.length; i++)
 	{
-		//Output folder
-		var folder = path.join(output, cat[i]);
+		//Get the icons
+		gulp.src(src, { base: 'icons' })
 
-		//Create the category folder
-		mkdirp.sync(folder);
+		//For each, replace the color
+		.pipe(replace(regexp, colors[i].hex))
 
-		//Read all the icons
-		for(var j = 0; j < icons[cat[i]].length; j++)
-		{
-			//Get the icon
-			var ic = path.join('./icons/', cat[i], icons[cat[i]][j] + '.svg');
+		//Rename the file
+		.pipe(rename({ suffix: '_' + colors[i].name }))
 
-			//Get the icon content
-			var content = fs.readFileSync(ic, 'utf8');
-
-			//Replace for each color
-			for(var k = 0; k < colors.length; k++)
-			{
-				//Create the regexp
-				var regexp = new RegExp('#000000', 'g');
-
-				//Replace the icon content
-				var cont2 = content.replace(regexp, colors[k].hex);
-
-				//Icon name
-				var name = icons[cat[i]][j] + '_' + colors[k].name + '.svg';
-
-				//Save the icon
-				fs.writeFileSync(path.join(folder, name), cont2, 'utf8');
-			}
-
-			//Icon completed, continue
-		}
+		//Output
+		.pipe(gulp.dest(output));
 	}
-
-	//Done
-}
-
-//Exports to node
-module.exports = BuildIcons;
+};
