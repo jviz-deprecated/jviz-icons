1b5b36415501747c7b8c4430bbd32061aca8d05b
jmjuanes <josemijuanes@gmail.com>
2016-04-05 11:58:15 +0200

    Update build icons task

--- tasks/build-icons.js
+++ tasks/build-icons.js
@@ -3,30 +3,27 @@ var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 
 //Function for build the icons
-module.exports = function(gulp, icons, colors, output)
+module.exports = function(gulp, config, icons)
 {
 	//Create the regexp
 	var regexp = new RegExp('#000000', 'g');
 
-	//Icons source
-	var src = [];
-
-	//Read all the icons
-	for(var i = 0; i < icons.length; i++){ src.push('./icons/' + icons[i]); }
+	//Parse all the icons path
+	for(var i = 0; i < icons.length; i++){ icons[i] = './svg/' + icons[i]; }
 
 	//Read all the colors
-	for(var i = 0; i < colors.length; i++)
+	for(var i = 0; i < config.colors.length; i++)
 	{
 		//Get the icons
-		gulp.src(src, { base: 'icons' })
+		gulp.src(icons, { base: 'svg' })
 
 		//For each, replace the color
-		.pipe(replace(regexp, colors[i].hex))
+		.pipe(replace(regexp, config.colors[i].hex))
 
 		//Rename the file
-		.pipe(rename({ suffix: '_' + colors[i].name }))
+		//.pipe(rename({ suffix: '_' + colors[i].name }))
 
 		//Output
-		.pipe(gulp.dest(output));
+		.pipe(gulp.dest(config.dest + config.colors[i].name + '/'));
 	}
 };
