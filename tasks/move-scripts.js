f0785cc1dcac03011cba3897333e279e541b36ba
jmjuanes <josemijuanes@gmail.com>
2016-04-05 15:16:03 +0200

    Update scripts source location

--- tasks/move-scripts.js
+++ tasks/move-scripts.js
@@ -5,7 +5,7 @@ var replace = require('gulp-replace');
 module.exports = function(gulp, config){
 
 	//Get the scripts
-	gulp.src([ './js/**/*.js' ])
+	gulp.src([ './docs/js/**/*.js' ])
 
 	//Save to the output folder
 	.pipe(gulp.dest(config.dest + 'js'));
