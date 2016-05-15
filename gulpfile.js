d06027e7577a4e829da2bc5d514e5a440761fd93
jmjuanes <josemijuanes@gmail.com>
2016-05-15 17:40:17 +0200

    Edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp');
 var replace = require('gulp-replace');
 var rename = require('gulp-rename');
 
-//Import config
-var Config = require('./config.json');
+//Import colors
+var Colors = require('./colors.json');
 
 //Build the icons
 gulp.task('build', function(){
@@ -17,26 +17,39 @@ gulp.task('build', function(){
 	for(var i = 0; i < icons.length; i++){ icons[i] = './svg/' + icons[i]; }
 
 	//Read all the colors
-	for(var i = 0; i < Config.colors.length; i++)
+	for(var i = 0; i < Colors.length; i++)
 	{
 		//Get the icons
 		gulp.src(icons, { base: 'svg' })
 
 		//Replace the first color
-		.pipe(replace(/#000000/g, Config.colors[i].hex))
+		.pipe(replace(/#000000/g, Colors[i].hex))
 
 		//Replace the second color
-		.pipe(replace(/#000/g, Config.colors[i].hex))
+		.pipe(replace(/#000/g, Colors[i].hex))
 
 		//Rename the file
 		//.pipe(rename({ suffix: '_' + colors[i].name }))
 
 		//Output
-		.pipe(gulp.dest(Config.dest + Config.colors[i].name + '/'));
+		.pipe(gulp.dest('./dist/' + Colors[i].name + '/'));
 	}
 
 });
 
+//Move to the app engine folder
+gulp.task('move', function(){
+
+	//Select all the files
+	gulp.src(['./dist/**/*'], { base: 'dist' })
+
+	//Move to the app engine folder
+	.pipe(gulp.dest('./appengine/'));
+	
+});
+
+//App engine task
+gulp.task('appengine', [ 'build', 'move' ]);
 
 //Default task
 gulp.task('default', ['build']);
